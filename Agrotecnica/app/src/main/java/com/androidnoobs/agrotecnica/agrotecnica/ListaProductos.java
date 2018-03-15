788d118c658e4aab0f9eeba20bdd94ecfa9eb76e
GemmiPower <37400612+GemmiPower@users.noreply.github.com>
2018-03-15 13:26:54 +0100

    Gemma02
    listaproductos

--- Agrotecnica/app/src/main/java/com/androidnoobs/agrotecnica/agrotecnica/ListaProductos.java
+++ Agrotecnica/app/src/main/java/com/androidnoobs/agrotecnica/agrotecnica/ListaProductos.java
@@ -9,5 +9,8 @@ public class ListaProductos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_productos);
+
     }
+
+
 }
