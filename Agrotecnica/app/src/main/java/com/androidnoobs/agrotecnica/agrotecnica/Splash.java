2a580fee547a93cf8c3d9e15b4074f6c8084a269
Juan <muxacalma@gmail.com>
2018-03-15 12:32:49 +0100

    Juan06
    AHORA YA TIENE QUE IR

--- Agrotecnica/app/src/main/java/com/androidnoobs/agrotecnica/agrotecnica/Splash.java
+++ Agrotecnica/app/src/main/java/com/androidnoobs/agrotecnica/agrotecnica/Splash.java
@@ -18,32 +18,7 @@ public class Splash extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Set portrait orientation
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        // Hide title bar
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-
-        setContentView(R.layout.activity_splash);
-
-        TimerTask task = new TimerTask() {
-            @Override
-            public void run() {
-
-                // Start the next activity
-                Intent mainIntent = new Intent().setClass(
-                        Splash.this, Usuario.class);
-                startActivity(mainIntent);
-
-                // Close the activity so the user won't able to go back this
-                // activity pressing Back button
-                finish();
-            }
-        };
-
-        // Simulate a long loading process on application startup.
-        Timer timer = new Timer();
-        timer.schedule(task, SPLASH_SCREEN_DELAY);
+        
     }
 
 }
--- Agrotecnica/app/src/main/java/com/androidnoobs/agrotecnica/agrotecnica/Usuario.java
+++
@@ -1,8 +0,0 @@
-package com.androidnoobs.agrotecnica.agrotecnica;
-
-/**
- * Created by portatil on 15/03/2018.
- */
-
-public class Usuario {
-}
