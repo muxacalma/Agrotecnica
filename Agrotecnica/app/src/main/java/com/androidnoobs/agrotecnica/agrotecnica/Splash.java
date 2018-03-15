b3968753dd6b0465bf8d55e58a9805dab6198355
BNKTOPR <bnktopr@gmail.com>
2018-03-15 12:24:14 +0100

    Victoria01
    Meto mis cosillas

--- Agrotecnica/app/src/main/java/com/androidnoobs/agrotecnica/agrotecnica/Splash.java
+++ Agrotecnica/app/src/main/java/com/androidnoobs/agrotecnica/agrotecnica/Splash.java
@@ -1,13 +1,49 @@
 package com.androidnoobs.agrotecnica.agrotecnica;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
-public class Splash extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Splash extends Activity {
+
+    // Set the duration of the splash screen
+    private static final long SPLASH_SCREEN_DELAY = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set portrait orientation
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        // Hide title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_splash);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+
+                // Start the next activity
+                Intent mainIntent = new Intent().setClass(
+                        Splash.this, Usuario.class);
+                startActivity(mainIntent);
+
+                // Close the activity so the user won't able to go back this
+                // activity pressing Back button
+                finish();
+            }
+        };
+
+        // Simulate a long loading process on application startup.
+        Timer timer = new Timer();
+        timer.schedule(task, SPLASH_SCREEN_DELAY);
     }
+
 }
---
+++ Agrotecnica/app/src/main/java/com/androidnoobs/agrotecnica/agrotecnica/Usuario.java
@@ -0,0 +1,8 @@
+package com.androidnoobs.agrotecnica.agrotecnica;
+
+/**
+ * Created by portatil on 15/03/2018.
+ */
+
+public class Usuario {
+}
