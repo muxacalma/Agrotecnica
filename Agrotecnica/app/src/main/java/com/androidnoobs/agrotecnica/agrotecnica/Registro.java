5a9127f2e36d724930015480a972238d9a913b9a
BNKTOPR <bnktopr@gmail.com>
2018-03-16 14:07:09 +0100

    Victoria12
    Firebase creado

--- Agrotecnica/app/src/main/java/com/androidnoobs/agrotecnica/agrotecnica/Registro.java
+++ Agrotecnica/app/src/main/java/com/androidnoobs/agrotecnica/agrotecnica/Registro.java
@@ -1,14 +1,25 @@
 package com.androidnoobs.agrotecnica.agrotecnica;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Registro extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,39 +27,42 @@ public class Registro extends AppCompatActivity {
         setContentView(R.layout.activity_registro);
     }
 
-    public void guardad(View v){
-        String nombre=((EditText)findViewById(R.id.nombre)).getText().toString();
-        String apellidos=((EditText)findViewById(R.id.apellidos)).getText().toString();
-        String mail=((EditText)findViewById(R.id.email)).getText().toString();
-        String contraseña=((EditText)findViewById(R.id.contraseña)).getText().toString();
-        String contraseña_encrip=md5(contraseña);
 
 
-        //FALTA HACER EL SERVICE
 
-        finish();
+    public void registroFirebase(View v) {
+
+        final String email = ((EditText) findViewById(R.id.email)).getText().toString();
+        final String password = ((EditText) findViewById(R.id.contraseña)).getText().toString();
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            // intent a categorias
+                            Intent in=new Intent(Registro.this,Categorias.class);
+                            startActivity(in);
+                            // updateUI(user);
+
+                            SharedPreferences sp=Registro.this.getSharedPreferences("email", Context.MODE_PRIVATE);
+                            SharedPreferences.Editor editor=sp.edit();
+                            editor.putString("email",email);
+                            editor.putString("contraseña",password);
+                        } else {
+                            // If sign in fails, display a message to the user.
+
+                            Toast.makeText(Registro.this, "No se ha podido realizar autentificación.",
+                                    Toast.LENGTH_SHORT).show();
+//                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
     }
-    public static final String md5(final String contraseña) {
-        final String MD5 = "MD5";
-        try {
-            // Create MD5 Hash
-            MessageDigest digest = java.security.MessageDigest.getInstance(MD5);
-            digest.update(contraseña.getBytes());
-            byte messageDigest[] = digest.digest();
 
-            // Create Hex String
-            StringBuilder hexString = new StringBuilder();
-            for (byte aMessageDigest : messageDigest) {
-                String h = Integer.toHexString(0xFF & aMessageDigest);
-                while (h.length() < 2)
-                    h = "0" + h;
-                hexString.append(h);
-            }
-            return hexString.toString();
 
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        return "";
-    }
 }
