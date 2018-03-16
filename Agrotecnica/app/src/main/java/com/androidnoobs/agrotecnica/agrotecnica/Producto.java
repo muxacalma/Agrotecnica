8ca0a5ea76fae48b0262fd2f2e6bd47e35e051ef
GemmiPower <37400612+GemmiPower@users.noreply.github.com>
2018-03-16 13:08:54 +0100

    Gemma04
    Clase Producto implementada

--- Agrotecnica/app/src/main/java/com/androidnoobs/agrotecnica/agrotecnica/Producto.java
+++ Agrotecnica/app/src/main/java/com/androidnoobs/agrotecnica/agrotecnica/Producto.java
@@ -5,6 +5,79 @@ package com.androidnoobs.agrotecnica.agrotecnica;
  */
 
 public class Producto {
+    private int id;
+    private String nombre;
+    private String descripcion;
+    private String precio;
+    private String imagen;
+    private int stock;
+    private String categoria;
+
+    public Producto (int id, String nombre, String descripcion, String precio, String imagen, int stock, String categoria){
+        this.id=id;
+        this.nombre=nombre;
+        this.descripcion=descripcion;
+        this.precio=precio;
+        this.imagen=imagen;
+        this.stock=stock;
+        this.categoria=categoria;
+    }
 
 
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id=id;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    }
+    public String getDescripcion(){
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion){
+        this.descripcion=descripcion;
+    }
+
+    public String getPrecio(){
+        return precio;
+    }
+
+    public void setPrecio(String precio){
+        this.precio=precio;
+    }
+
+    public String getImagen(){
+        return imagen;
+    }
+
+    public void setImagen(String imagen){
+        this.imagen=imagen;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock){
+        this.stock=stock;
+    }
+
+    public String getCategoria(){
+        return categoria;
+    }
+
+    public void setCategoria(String categoria){
+        this.categoria=categoria;
+    }
+    
 }
