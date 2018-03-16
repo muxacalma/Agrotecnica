fd3e5dba3f933005a5607ea77b55d91ad5379e96
BNKTOPR <bnktopr@gmail.com>
2018-03-16 14:32:21 +0100

    Victoria13
    google in

--- Agrotecnica/app/src/main/java/com/androidnoobs/agrotecnica/agrotecnica/Login.java
+++ Agrotecnica/app/src/main/java/com/androidnoobs/agrotecnica/agrotecnica/Login.java
@@ -111,5 +111,41 @@ public class Login extends AppCompatActivity {
             }
         });
     }
-  
+
+    /*En oncreate
+     // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+     */
+  //mGoogleApiClient=new GoogleApiClient.Builder(getActivity()).enableAutoManage(this,this).addApi
+    // (Auth.GOOGLE_SIGN_IN_API, gso).build();
+    /*
+hdr_strong
+
+content_copy
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            } else {
+                // Google Sign In failed, update UI appropriately
+                // ...
+            }
+        }
+    }*/
 }
