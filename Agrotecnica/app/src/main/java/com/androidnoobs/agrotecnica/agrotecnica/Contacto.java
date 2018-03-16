9d5f2587d3b9f09081a42173f6c255eff0b07bfe
AntonioCerro <37400637+AntonioCerro@users.noreply.github.com>
2018-03-16 14:11:23 +0100

    Momo05
    Avance

--- Agrotecnica/app/src/main/java/com/androidnoobs/agrotecnica/agrotecnica/Contacto.java
+++ Agrotecnica/app/src/main/java/com/androidnoobs/agrotecnica/agrotecnica/Contacto.java
@@ -1,13 +1,24 @@
 package com.androidnoobs.agrotecnica.agrotecnica;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Contacto extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+//<meta-data android:name="com.google.android.geo.API_KEY" android:value="your API key"/>
+
+public class Contacto extends FragmentActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacto);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
     }
 }
