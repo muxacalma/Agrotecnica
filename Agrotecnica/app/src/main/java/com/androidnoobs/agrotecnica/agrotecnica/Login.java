70fbe86b8c949f36f278686152037f390c109c51
BNKTOPR <bnktopr@gmail.com>
2018-03-16 09:54:00 +0100

    victoria06

--- Agrotecnica/app/src/main/java/com/androidnoobs/agrotecnica/agrotecnica/Login.java
+++ Agrotecnica/app/src/main/java/com/androidnoobs/agrotecnica/agrotecnica/Login.java
@@ -1,7 +1,13 @@
 package com.androidnoobs.agrotecnica.agrotecnica;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Login extends AppCompatActivity {
 
@@ -10,4 +16,42 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    public void entrar(View v){
+        String mail=((EditText)findViewById(R.id.email)).getText().toString();
+        String contraseña=((EditText)findViewById(R.id.contraseña)).getText().toString();
+        String contraseña_encrip=md5(contraseña);
+    }
+
+    public static final String md5(final String contraseña) {
+        final String MD5 = "MD5";
+        try {
+            // Create MD5 Hash
+            MessageDigest digest = java.security.MessageDigest.getInstance(MD5);
+            digest.update(contraseña.getBytes());
+            byte messageDigest[] = digest.digest();
+
+            // Create Hex String
+            StringBuilder hexString = new StringBuilder();
+            for (byte aMessageDigest : messageDigest) {
+                String h = Integer.toHexString(0xFF & aMessageDigest);
+                while (h.length() < 2)
+                    h = "0" + h;
+                hexString.append(h);
+            }
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public void registrar(View v){
+        /*Intent in=new Intent(this,Registrar.class);
+        startActivity(in);*/
+    }
+
+
+
 }
