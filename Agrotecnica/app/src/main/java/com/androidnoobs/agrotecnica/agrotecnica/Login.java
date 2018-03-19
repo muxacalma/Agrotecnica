a9348a6580b2462bc7ce8225e3fe8b8c00367e5d
BNKTOPR <bnktopr@gmail.com>
2018-03-19 09:38:23 +0100

    Victoria15
    Google services añadido para iniciar sesion

--- Agrotecnica/app/src/main/java/com/androidnoobs/agrotecnica/agrotecnica/Login.java
+++ Agrotecnica/app/src/main/java/com/androidnoobs/agrotecnica/agrotecnica/Login.java
@@ -6,6 +6,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -17,18 +18,40 @@ import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+    private static final int RC_SIGN_IN = 6321;
     private FirebaseAuth mAuth;
+    private GoogleApiClient mGoogleApiClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+// ...
         mAuth = FirebaseAuth.getInstance();
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mAuth = FirebaseAuth.getInstance();
+        mGoogleApiClient= new GoogleApiClient.Builder(Login.this)
+                .enableAutoManage(Login.this,Login.this).addApi(Auth.GOOGLE_SIGN_IN_API,gso).build();
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         FacebookSdk.sdkInitialize(getApplicationContext());
@@ -74,14 +97,6 @@ public class Login extends AppCompatActivity {
     }
 
 
-    @Override
-
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        CallbackManager callbackManager = CallbackManager.Factory.create();
-        callbackManager.onActivityResult(requestCode, resultCode, data);
-        super.onActivityResult(requestCode, resultCode, data);
-    }
-
     public void loginFirebase(View v) {
         final String email = ((EditText) findViewById(R.id.email)).getText().toString();
         final String password = ((EditText) findViewById(R.id.contraseña)).getText().toString();
@@ -110,23 +125,11 @@ public class Login extends AppCompatActivity {
                 // ...
             }
         });
+
     }
 
-    /*En oncreate
-     // Configure Google Sign In
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-     */
-  //mGoogleApiClient=new GoogleApiClient.Builder(getActivity()).enableAutoManage(this,this).addApi
-    // (Auth.GOOGLE_SIGN_IN_API, gso).build();
-    /*
-hdr_strong
 
-content_copy
-
-    private void signIn() {
+    public void signIn(View v) {
         Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
@@ -147,5 +150,44 @@ content_copy
                 // ...
             }
         }
-    }*/
+        else{
+            CallbackManager callbackManager = CallbackManager.Factory.create();
+            callbackManager.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        final String TAG="proyecto";
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            Intent in=new Intent(Login.this,Categorias.class);
+                            startActivity(in);
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(Login.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
