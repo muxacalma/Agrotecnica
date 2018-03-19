4824ae64bb1dc53a5dcdea745e4887869216ce88
BNKTOPR <bnktopr@gmail.com>
2018-03-19 13:05:45 +0100

    Victoria20
    logout

--- Agrotecnica/app/src/main/java/com/androidnoobs/agrotecnica/agrotecnica/Gracias.java
+++ Agrotecnica/app/src/main/java/com/androidnoobs/agrotecnica/agrotecnica/Gracias.java
@@ -1,7 +1,10 @@
 package com.androidnoobs.agrotecnica.agrotecnica;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import com.facebook.login.LoginManager;
 
 public class Gracias extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class Gracias extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gracias);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_poweroff:
+               //logout
+                LoginManager.getInstance().logOut();
+                return true;
+
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
