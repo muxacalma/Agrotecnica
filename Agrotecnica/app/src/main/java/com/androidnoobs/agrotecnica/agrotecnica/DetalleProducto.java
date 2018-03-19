270b49b5494b308d0a3cbb56c764d2b15f203751
AdelinaMB <lascosicasdeade@gmail.com>
2018-03-19 12:52:36 +0100

    AdelinaMB04
    añado textView e ImageView en java para cambiar nombre, precio, stock e imagen de productos.

--- Agrotecnica/app/src/main/java/com/androidnoobs/agrotecnica/agrotecnica/DetalleProducto.java
+++ Agrotecnica/app/src/main/java/com/androidnoobs/agrotecnica/agrotecnica/DetalleProducto.java
@@ -4,20 +4,34 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class DetalleProducto extends AppCompatActivity {
+
+    TextView nombre, precio, stock, descripciónProducto;
+    ImageView Imagen;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_producto);
+
+        nombre = findViewById(R.id.dpNombreProducto);
+        precio = findViewById(R.id.dpPrecio);
+        stock = findViewById(R.id.dpStock);
+        descripciónProducto = findViewById(R.id.dpDescricionProducto);
+
+        Imagen = findViewById(R.id.dpImagen);
+
     }
 
 
     public void realizarCompra(View v){
 
-        Toast.makeText(getApplicationContext(), "ENTRA EN COMPRAR", Toast.LENGTH_SHORT).show();
+
         Intent i = new Intent(this, Gracias.class);
         startActivity(i);
     }
