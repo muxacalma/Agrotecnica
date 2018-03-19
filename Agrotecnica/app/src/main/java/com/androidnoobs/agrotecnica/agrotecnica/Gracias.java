e5b50225bbf6d337b328da12331a89f23227f41b
BNKTOPR <bnktopr@gmail.com>
2018-03-19 13:43:26 +0100

    Victoria22
    cerrar sesion en clase gracias

--- Agrotecnica/app/src/main/java/com/androidnoobs/agrotecnica/agrotecnica/Gracias.java
+++ Agrotecnica/app/src/main/java/com/androidnoobs/agrotecnica/agrotecnica/Gracias.java
@@ -1,7 +1,11 @@
 package com.androidnoobs.agrotecnica.agrotecnica;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.facebook.login.LoginManager;
@@ -9,14 +13,24 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class Gracias extends AppCompatActivity {
 
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gracias);
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.actionbar, menu);
+        return super.onCreateOptionsMenu(menu);
+
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
         switch (item.getItemId()) {
             case R.id.action_poweroff:
                //logout
@@ -24,6 +38,8 @@ public class Gracias extends AppCompatActivity {
 
                FirebaseAuth.getInstance().signOut();
                //intent a login
+                Intent in=new Intent(this,Login.class);
+                startActivity(in);
                 return true;
 
 
