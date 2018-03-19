cc36024d74c25c39c6badaf14aac6b464395e757
Juan <muxacalma@gmail.com>
2018-03-19 11:49:58 +0100

    Juan16
    Funcionalidad Botón compra detalle producto

--- Agrotecnica/app/src/main/java/com/androidnoobs/agrotecnica/agrotecnica/DetalleProducto.java
+++ Agrotecnica/app/src/main/java/com/androidnoobs/agrotecnica/agrotecnica/DetalleProducto.java
@@ -1,7 +1,10 @@
 package com.androidnoobs.agrotecnica.agrotecnica;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class DetalleProducto extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class DetalleProducto extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_producto);
+    }
+
+
+    public void realizarCompra(View v){
+
+        Toast.makeText(getApplicationContext(), "ENTRA EN COMPRAR", Toast.LENGTH_SHORT).show();
+        Intent i = new Intent(this, Gracias.class);
+        startActivity(i);
     }
 }
