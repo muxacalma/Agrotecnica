13745e3cc76626663da81bb7efd1a9d223d84cae
AntonioCerro <37400637+AntonioCerro@users.noreply.github.com>
2018-03-19 09:16:28 +0100

    Momo07
    Cambio en Manifest de map

--- Agrotecnica/app/src/main/java/com/androidnoobs/agrotecnica/agrotecnica/Contacto.java
+++ Agrotecnica/app/src/main/java/com/androidnoobs/agrotecnica/agrotecnica/Contacto.java
@@ -4,21 +4,36 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 //<meta-data android:name="com.google.android.geo.API_KEY" android:value="your API key"/>
 
 public class Contacto extends FragmentActivity implements OnMapReadyCallback {
 
+    private GoogleMap mMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacto);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
 
+        LatLng albacete = new LatLng(38.9738639, -1.8530662);
+        mMap.addMarker(new MarkerOptions().position(albacete).title("Marker in Albacete"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(albacete));
     }
 }
