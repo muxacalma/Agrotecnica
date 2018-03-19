0a31374ddcfc823b754f8cee77932e4c7ea6d313
BNKTOPR <bnktopr@gmail.com>
2018-03-19 14:01:01 +0100

    Victoria23
    logout en contacto

--- Agrotecnica/app/src/main/java/com/androidnoobs/agrotecnica/agrotecnica/Contacto.java
+++ Agrotecnica/app/src/main/java/com/androidnoobs/agrotecnica/agrotecnica/Contacto.java
@@ -1,16 +1,21 @@
 package com.androidnoobs.agrotecnica.agrotecnica;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import com.facebook.login.LoginManager;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
 
 //<meta-data android:name="com.google.android.geo.API_KEY" android:value="your API key"/>
 
@@ -26,6 +31,35 @@ public class Contacto extends FragmentActivity implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.actionbar, menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_poweroff:
+                //logout
+                LoginManager.getInstance().logOut();
+
+                FirebaseAuth.getInstance().signOut();
+                //intent a login
+                Intent in=new Intent(this,Login.class);
+                startActivity(in);
+                return true;
+
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 
     @Override
